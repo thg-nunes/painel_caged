5518b524ec5167a1812f064ba34598eea5d975af
thiago <desenvolvedor.nunes@gmail.com>
2022-03-07 10:31:56 -0300

    adiciona styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme';
+import { GlobalStyles } from './styles/global-styles';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+export const theme = {
+  cores: {
+    headerBg: '#263544',
+  },
+  font: {
+    cores: {
+      white: '#FFFFFF',
+    },
+    background: '#0A6F89',
+    family: {
+      default: "'Fira Sans', sans-serif;",
+    },
+    sizes: {
+      default: '2.5rem',
+      title_Graficos: '2rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacings: {
+    xsmall: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
+}
