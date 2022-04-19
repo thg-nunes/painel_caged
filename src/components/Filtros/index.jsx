413d1eac4a0b0a7585c84cdf85d112b38cd63a64
thiago <desenvolvedor.nunes@gmail.com>
2022-04-19 17:29:06 -0300

    muda funcao de mostrar e esconder filtros

--- src/components/Filtros/index.jsx
+++ src/components/Filtros/index.jsx
@@ -138,11 +138,6 @@ export const Filtros = () => {
   return (
     <div className='containerFiltros' id='all_filters'>
       <p className="texto-filtros">Filtros:</p>
-      <svg className='close-menu' xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="#000000"
-        onClick={() => {
-          document.getElementById('all_filters').style.display = 'none'
-        }}
-      ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
       <div className='containerMultiSelect'>
         <div>
           <p>Ano:</p>
--- src/components/dataHeader/index.jsx
+++ src/components/dataHeader/index.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import logo from './Governo-Maranhao-MA.jpg'
 import './style.css'
 
 export const DataHeader = () => {
+  const [menuBlock, setMenuBlock] = useState(false)
   return (
     <section className='header-mobile'>
       <div className='data'>
@@ -10,7 +12,8 @@ export const DataHeader = () => {
       </div>
         <svg className='menu-mobile' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"
           onClick={() => {
-            document.getElementById('all_filters').style.display = 'block'
+            setMenuBlock(!menuBlock)
+            document.getElementById('all_filters').style.display = menuBlock ? 'block' : 'none'
           }}
         ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
     </section>
