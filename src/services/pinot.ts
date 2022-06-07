90056c89bb6510a1f29d7e2a981b46d53fd446b3
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-06-07 15:10:15 -0300

    fix: remove barra da url_api

--- src/services/pinot.ts
+++ src/services/pinot.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url_api = 'http://dados.jucema.ma.gov.br/'
+const url_api = 'http://dados.jucema.ma.gov.br'
 
 export const getDadosGraficos = async (classificacao, filtros) => {
   return await axios({
