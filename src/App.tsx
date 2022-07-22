0e7a599fc66165d843b841ee8a84248284bf6cfb
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-22 16:07:36 -0300

    feature: adiciona cache nos dados das tabelas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Redirect, Switch, Route } from 'react-router'
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
 import { ContextProvider } from './contexts/contextProvider';
 import { Home } from './templates/home'
+
+const client = new QueryClient()
 
 class App extends React.PureComponent {
   render() {
     return (
-      <ContextProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/empregos/formais" component={Home} />
-            <Redirect from='/' to="/empregos/formais"/>
-            <Redirect from='*' to="/empregos/formais"/>
-          </Switch>
-        </BrowserRouter>
-      </ContextProvider>
+      <QueryClientProvider client={client}>
+        <ContextProvider>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/empregos/formais" component={Home} />
+              <Redirect from='/' to="/empregos/formais"/>
+              <Redirect from='*' to="/empregos/formais"/>
+            </Switch>
+          </BrowserRouter>
+        </ContextProvider>
+      </QueryClientProvider>
     )
   }
 }
